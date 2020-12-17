177c72bd8bf2740975fed24316fb07b86720d515
bam8r <bam8r4@mst.edu>
2020-12-16 23:05:00 -0600

    added more users

--- tbomb.py
+++ tbomb.py
@@ -1,20 +1,45 @@
 import smtplib
 
-sender = 'youremail@gmail.com'
-rec = 'theirnumber@provider'
+sender1 = 'youremail@gmail.com'
+sender2 = 'youremail@gmail.com'
+sender3 = 'youremail@gmail.com'
+sender4 = 'youremail@gmail.com'
+sender5 = 'youremail@gmail.com'
 
 password = 'yourEmailPassword'
+
+rec = 'theirnumber@provider'
 
 #Message you want to send.
 message = """Ham bone chicken wing"""
 
-s = smtplib.SMTP("smtp.gmail.com", 587)
-s.starttls()
-s.login(sender,password)
+s1 = smtplib.SMTP("smtp.gmail.com", 587)
+s1.starttls()
+s1.login(sender1,password)
+
+s2 = smtplib.SMTP("smtp.gmail.com", 587)
+s2.starttls()
+s2.login(sender2,password)
+
+s3 = smtplib.SMTP("smtp.gmail.com", 587)
+s3.starttls()
+s3.login(sender3,password)
+
+s4 = smtplib.SMTP("smtp.gmail.com", 587)
+s4.starttls()
+s4.login(sender4,password)
+
+s5 = smtplib.SMTP("smtp.gmail.com", 587)
+s5.starttls()
+s5.login(sender5,password)
 
 numberMessagesToSend = 15
 
 for x in range(0,numberMessagesToSend):
-    s.sendmail(sender, rec, message)
+    s1.sendmail(sender1, rec, message)
+    s2.sendmail(sender2, rec, message)
+    s3.sendmail(sender3, rec, message)
+    s4.sendmail(sender4, rec, message)
+    s5.sendmail(sender5, rec, message)
 
 s.quit()
